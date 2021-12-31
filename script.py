2b1a3ea3333f0ff174aef387e3c01969d796a2c9
CompassGenius <96708902+CompassGenius@users.noreply.github.com>
2021-12-31 23:29:57 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,6 @@
 class Script(object):
     START_TXT = """<b>@cinema_kotaka..!</b>"""
-    FORCESUB_TXT = 
+    FORCESUB_TXT = "You Need To Join Our Channel and Press Refresh Button to get the file\n🍎ഫയലുകൾ ലഭിക്കുന്നതിനായി  നിങ്ങൾ ഞങ്ങളുടെ ചാനലിൽ join ചെയ്യണം\n🍎 ശേഷം refresh button അമർത്തുക",
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Join Channel",url="https://t.me/Dustinreq")],
+                                                       [InlineKeyboardButton(text="Refresh", url=f"https://t.me/{me.username}?start={file_uid}")]]),
+                    reply_to_message_id=update.message_id
